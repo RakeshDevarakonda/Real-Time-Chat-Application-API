0e70608757589299f0c5f3bdd69947876c6ae6c5
Rakesh <rakeshdevarakonda2000@gmail.com>
2025-07-23 17:07:46 +0530

    github actions routes test 1

--- redis-config.js
+++ redis-config.js
@@ -1,7 +1,8 @@
 import Redis from "ioredis";
 
-export const redisClient = new Redis("redis://localhost:6379");
+export const redisClient = new Redis(process.env.redis);
 
 redisClient.on("connect", () => console.log("✅ Connected to Redis"));
 redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
+
 
